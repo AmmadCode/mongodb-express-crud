62b82b7fb25c062933152a63d3e2fd5d22369b17
Muhammad Ammad <kingworkgroup@gmail.com>
2025-07-24 17:29:22 +0500

    Intial setup

---
+++ index.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const path = require('path');
+const Chat = require("./models/chat.js");
+require('dotenv').config();
+
+
+app.set("views", path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+// MongoDB Connection
+const username = process.env.MONGODB_USERNAME;
+const password = process.env.MONGODB_PASSWORD;
+const cluster = process.env.MONGODB_CLUSTER;
+const database = process.env.MONGODB_DATABASE;
+const connectionString = `mongodb+srv://${username}:${password}@${cluster}/${database}?retryWrites=true&w=majority&appName=Cluster0`;
+
+mongoose.connect(connectionString)
+  .then(() => {
+    console.log('Successfully connected to MongoDB Atlas!');
+  })
+  .catch((error) => {
+    console.error('Error connecting to MongoDB Atlas:', error);
+  });
+
+
+
+let chat1 = new Chat({
+    from : "Ammad",
+    to : "Midhat",
+    msg: "I Love you!",
+    created_at : new Date()
+
+});
+
+chat1.save()
+.then((res) => console.log(res));
+
+
+
+app.listen(8080, () => {
+  console.log('Server is running on port 8080');
+});
