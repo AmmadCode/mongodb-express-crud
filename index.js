1d55dcaba5f16ed5b3c42f89c24ca12d630c57c2
Muhammad Ammad <kingworkgroup@gmail.com>
2025-07-25 17:09:23 +0500

    make Edit Route

--- index.js
+++ index.js
@@ -63,6 +63,14 @@ app.post("/chats", (req,res) => {
   res.redirect("/chats")
 
 
+});
+
+
+
+app.get("/chats/:id/edit", async (req,res) => {
+  let { id } = req.params;
+  let chat = await Chat.findById(id)
+  res.render("edit.ejs", {chat})
 })
 
 
