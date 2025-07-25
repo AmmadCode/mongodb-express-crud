bb055e8180776e4ab7c022badb33d38ae521a6a9
Muhammad Ammad <kingworkgroup@gmail.com>
2025-07-25 18:06:46 +0500

    crud app Complete with mongo

--- index.js
+++ index.js
@@ -4,12 +4,15 @@ const mongoose = require('mongoose');
 const path = require('path');
 const Chat = require("./models/chat.js");
 require('dotenv').config();
+const methodOverride = require("method-override");
+
 
 
 app.set("views", path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(express.urlencoded({extended: true }));
+app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride("_method"));
 
 
 
@@ -31,24 +34,24 @@ mongoose.connect(connectionString)
 
 
 
-app.get("/", (req,res) => {
-   res.render("main.ejs")
+app.get("/", (req, res) => {
+  res.render("main.ejs")
 });
 
 
-app.get("/chats", async (req,res) => {
-   let datas = await Chat.find();
-   res.render("index.ejs", { datas })
+app.get("/chats", async (req, res) => {
+  let datas = await Chat.find();
+  res.render("index.ejs", { datas })
 
 });
 
-app.get("/chats/new", (req,res) => {
+app.get("/chats/new", (req, res) => {
   res.render("new.ejs")
 });
 
 
-app.post("/chats", (req,res) => {
-  let {from,to,msg} = req.body;
+app.post("/chats", (req, res) => {
+  let { from, to, msg } = req.body;
   let newChat = new Chat({
     from: from,
     to: to,
@@ -67,10 +70,27 @@ app.post("/chats", (req,res) => {
 
 
 
-app.get("/chats/:id/edit", async (req,res) => {
+app.get("/chats/:id/edit", async (req, res) => {
   let { id } = req.params;
   let chat = await Chat.findById(id)
-  res.render("edit.ejs", {chat})
+  res.render("edit.ejs", { chat })
+})
+
+app.put("/chats/:id", async (req, res) => {
+  let { id } = req.params;
+  let { msg : newMsg } = req.body;
+  let updatedChat = await Chat.findByIdAndUpdate(id, { msg: newMsg }, { runValidators: true, new: true });
+  console.log(updatedChat);
+  res.redirect("/chats");
+})
+
+
+app.delete("/chats/:id",  async (req,res) => {
+  let { id } = req.params;
+  let deleted =  await Chat.findByIdAndDelete(id);
+  res.redirect("/chats");
+
+
 })
 
 
